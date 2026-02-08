5485ea63a0c394120ac9c34d718c82ddff0a0218
tr-knodemy <tejasraj@knodemy.com>
2026-02-07 19:56:56 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,27 @@
 import discord
 from discord.ext import commands
-import logging
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
+secret_role = "Member"  # MUST match a role name in your server
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
 @bot.event
 async def on_member_join(member):
     await member.send(f"Welcome to the server {member.name}")
-    
+
 @bot.command()
 async def hello(ctx):
     await ctx.send(f"Hello {ctx.author.mention}!")
@@ -36,7 +47,7 @@ async def remove(ctx):
 @bot.command()
 async def dm(ctx, *, msg):
     await ctx.author.send(f"You said {msg}")
-    
+
 @bot.command()
 async def reply(ctx):
     await ctx.reply("This is a reply to your message!")
@@ -45,8 +56,7 @@ async def reply(ctx):
 async def poll(ctx, *, question):
     embed = discord.Embed(title="New Poll", description=question)
     poll_message = await ctx.send(embed=embed)
-    await poll_message.add_reaction(":)")
-    await poll_message.add_reaction(":(")
+    await poll_message.add_reaction("👍")
+    await poll_message.add_reaction("👎")
 
-
-
+bot.run(token)
